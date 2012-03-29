1e0be5253fb8603c06fcb014919b009f58cf31bb
Paula Peters <paula.peters@sri.com>
2012-03-29 09:34:40 -0400

    Removed FOO and added in CATALOG service.xml.

---
+++ portlet/course/course-portlet-service/src/main/java/org/nterlearning/datamodel/catalog/model/Component.java
@@ -0,0 +1,20 @@
+package org.nterlearning.datamodel.catalog.model;
+
+import com.liferay.portal.model.PersistedModel;
+
+/**
+ * The extended model interface for the Component service. Represents a row in the &quot;CATALOG_Component&quot; database table, with each column mapped to a property of this class.
+ *
+ * @author Brian Wing Shun Chan
+ * @see ComponentModel
+ * @see org.nterlearning.datamodel.catalog.model.impl.ComponentImpl
+ * @see org.nterlearning.datamodel.catalog.model.impl.ComponentModelImpl
+ * @generated
+ */
+public interface Component extends ComponentModel, PersistedModel {
+    /*
+     * NOTE FOR DEVELOPERS:
+     *
+     * Never modify this interface directly. Add methods to {@link org.nterlearning.datamodel.catalog.model.impl.ComponentImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+     */
+}
