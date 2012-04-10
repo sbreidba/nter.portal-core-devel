30cb430d37a8d5b6242c35b77557984a97ddb77c
John Deringer <john.deringer@gmail.com>
2012-04-10 12:04:05 -0700

    Moved Liferay property key to Constant class

---
+++ portlet/registry/src/main/java/org/nterlearning/service/registry/PropKeyConst.java
@@ -0,0 +1,13 @@
+package org.nterlearning.service.registry;
+
+/**
+ * @author <a href="mailto:john.deringer@sri.com">John Deringer</a>
+ *         Date: 4/10/12
+ */
+public class PropKeyConst {
+
+    public static final String BLACKLIST_DEFAULT_STATUS =
+            "org.nterlearning.blacklist.status";
+
+
+}
