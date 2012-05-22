a6c1a2b953df62fc78c4ab5ffa9895ba7b434df6
l.moulder.sri <l.moulder.sri@localhost>
2012-05-22 13:06:08 -0700

    Catalog : Configured ReportReaper to correctly use threads

---
+++ portlet/catalog/catalog-portlet/src/main/java/org/nterlearning/migration/model/UserRolesExtract.java
@@ -0,0 +1,62 @@
+/*
+ * National Training and Education Resource (NTER)
+ * Copyright (C) 2012 SRI International
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+package org.nterlearning.migration.model;
+
+
+/**
+ * User Role information extracted for migration
+ */
+public class UserRolesExtract {
+
+    private String ssoValue;
+    private String mapperType;
+    private String emailAddress;
+    private String roleName;
+
+    public String getSsoValue() {
+        return ssoValue;
+    }
+    public void setSsoValue(String newValue) {
+        ssoValue = newValue;
+    }
+
+    public String getMapperType() {
+        return mapperType;
+    }
+    public void setMapperType(String newValue) {
+        mapperType = newValue;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    public void setEmailAddress(String newValue) {
+        emailAddress = newValue;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+    public void setRoleName(String newValue) {
+        roleName = newValue;
+    }
+}
+
